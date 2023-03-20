6f54c3963d1d68d36af33ca69a52959000680027
veliciavv <100945245+veliciavv@users.noreply.github.com>
2023-03-20 19:32:25 +0800

    Add files via upload

---
+++ formm.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+Selamat Datang <?php echo $_POST["nama"]; ?> <br>
+NIM anda adalah <?php echo $_POST["nim"]; ?> <br>
+Anda berkuliah di Universitas <?php echo $_POST["univ"]; ?> <br>
+di Fakultas <?php echo $_POST["fakultas"]; ?> <br>
+dengan Program Studi <?php echo $_POST["prodi"]; ?>
+
+</body>
+</html>
